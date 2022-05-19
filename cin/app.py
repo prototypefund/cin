7ba61f98b7b23f506640c63a86c4dc34bbff487c
lluar <lluar@ccodein.org>
2022-05-19 17:50:58 +0200

    DEL: some imports

--- cin/app.py
+++ cin/app.py
@@ -1,10 +1,8 @@
-import kivy
-kivy.require('2.1.0') # replace with your current kivy version !
-
 from kivy.app import App as KVApp
 from kivy.uix.label import Label
 
 
 class App(KVApp):
     def build(self):
+        print('##########################')
         return Label(text='CIN app')
