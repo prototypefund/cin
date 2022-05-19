582d852ee1633f2bca446fa4f27e624f91162274
lluar <lluar@ccodein.org>
2022-05-19 17:45:07 +0200

    ADD: test App

--- cin/app.py
+++ cin/app.py
@@ -1,15 +1,10 @@
 import kivy
 kivy.require('2.1.0') # replace with your current kivy version !
 
-from kivy.app import App
+from kivy.app import App as KVApp
 from kivy.uix.label import Label
 
 
-class MyApp(App):
-
+class App(KVApp):
     def build(self):
-        return Label(text='Hello world')
-
-
-if __name__ == '__main__':
-    MyApp().run()
+        return Label(text='CIN app')
