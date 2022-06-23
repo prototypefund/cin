451c745c8bb54d76a43f8fc921acdb155cabc0be
lluar <lluar@ccodein.org>
2022-06-23 19:59:14 +0200

    ADD: SettingsScreen

---
+++ cin/uix/settings.py
@@ -0,0 +1,10 @@
+from kivymd.uix.screen import MDScreen
+from kivy.lang.builder import Builder
+from kivy.uix.screenmanager import Screen
+
+
+Builder.load_file('uix/settings.kv')
+
+
+class SettingsScreen(Screen):
+    pass
