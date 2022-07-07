646f50a3ce00e609efcff11d4bb50e0cac418416
lluar <lluar@ccodein.org>
2022-07-07 12:51:39 +0200

    MOD: ProductGroup model

--- cin/models.py
+++ cin/models.py
@@ -1,5 +1,6 @@
 """The module contains the database models."""
-from sqlalchemy import Column, JSON
+from sqlalchemy import Column, String, ForeignKey
+from sqlalchemy.orm import relationship
 from cin.database import Base
 
 
@@ -7,4 +8,8 @@ class ProductGroup(Base):
     """The model for product groups."""
     __tablename__ = 'product_groups'
 
-    data = Column(JSON)
+    group_id = Column(String, unique=True, nullable=False)
+    parent_id = Column(String, ForeignKey('product_groups.group_id'))
+    name = Column(String, nullable=False)
+    description = Column(String)
+    children = relationship('ProductGroup')
