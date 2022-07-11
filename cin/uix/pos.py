a759c6e1467118329e7cafb70303c1706a6161a1
lluar <lluar@ccodein.org>
2022-07-11 10:08:37 +0200

    FIX: layout error in Pos widget for small devices

--- cin/uix/pos.py
+++ cin/uix/pos.py
@@ -22,7 +22,7 @@ class PosLayoutDefault(MDBoxLayout):
     pass
 
 
-class PosTabSmall(MDFloatLayout, MDTabsBase):
+class PosTabSmall(MDBoxLayout, MDTabsBase):
     pass
 
 
@@ -32,41 +32,68 @@ class Pos(MDScreen):
 
         app = MDApp.get_running_app()
         app.bind(device=self.on_device)
-        self._layout_small = None
-        self._layout_default = None
+        self._layout_small = PosLayoutSmall()
+        self._products_tab_small = PosTabSmall(title='Products')
+        self._sale_tab_small = PosTabSmall(title='Sale')
+        self._layout_small.add_widget(self._products_tab_small)
+        self._layout_small.add_widget(self._sale_tab_small)
+        self._layout_default = PosLayoutDefault()
         self._sale = Sale()
         self._products = Products()
 
     def on_device(self, instance, value):
         if value == 'S':
-            if self._layout_default:
-                self._layout_default.remove_widget(self._products)
-                self._layout_default.remove_widget(self._sale)
-                self.remove_widget(self._layout_default)
+            if self._layout_small not in self.children:
+                if self._layout_default in self.children:
+                    self._layout_default.remove_widget(self._products)
+                    self._layout_default.remove_widget(self._sale)
+                    self.remove_widget(self._layout_default)
 
-            if not self._layout_small:
-                self._layout_small = PosLayoutSmall()
-                self._products_tab = PosTabSmall(title='Products')
-                self._products_tab.add_widget(self._products)
-                self._sale_tab = PosTabSmall(title='Sale')
-                self._sale_tab.add_widget(self._sale)
-
-            self._layout_small.add_widget(self._products_tab)
-            self._layout_small.add_widget(self._sale_tab)
-            self.add_widget(self._layout_small)
+                self._products_tab_small.add_widget(self._products)
+                self._sale_tab_small.add_widget(self._sale)
+                self.add_widget(self._layout_small)
 
         else:
-            if self._layout_small:
-                self._products_tab.remove_widget(self._products)
-                self._sale_tab.remove_widget(self._sale)
-                self.remove_widget(self._layout_small)
+            if self._layout_default not in self.children:
+                if self._layout_small in self.children:
+                    self._products_tab_small.remove_widget(self._products)
+                    self._sale_tab_small.remove_widget(self._sale)
+                    self.remove_widget(self._layout_small)
 
-            if not self._layout_default:
-                self._layout_default = PosLayoutDefault()
+                self._layout_default.add_widget(self._products)
+                self._layout_default.add_widget(self._sale)
+                self.add_widget(self._layout_default)
 
-            self._layout_default.add_widget(self._products)
-            self._layout_default.add_widget(self._sale)
-            self.add_widget(self._layout_default)
+    # def on_device(self, instance, value):
+    #     if value == 'S':
+    #         if self._layout_default:
+    #             self._layout_default.remove_widget(self._products)
+    #             self._layout_default.remove_widget(self._sale)
+    #             self.remove_widget(self._layout_default)
+    #
+    #         if not self._layout_small:
+    #             self._layout_small = PosLayoutSmall()
+    #             self._products_tab = PosTabSmall(title='Products')
+    #             self._products_tab.add_widget(self._products)
+    #             self._sale_tab = PosTabSmall(title='Sale')
+    #             self._sale_tab.add_widget(self._sale)
+    #
+    #         self._layout_small.add_widget(self._products_tab)
+    #         self._layout_small.add_widget(self._sale_tab)
+    #         self.add_widget(self._layout_small)
+    #
+    #     else:
+    #         if self._layout_small:
+    #             self._products_tab.remove_widget(self._products)
+    #             self._sale_tab.remove_widget(self._sale)
+    #             self.remove_widget(self._layout_small)
+    #
+    #         if not self._layout_default:
+    #             self._layout_default = PosLayoutDefault()
+    #
+    #         self._layout_default.add_widget(self._products)
+    #         self._layout_default.add_widget(self._sale)
+    #         self.add_widget(self._layout_default)
 
 
 class PosScreen(MDScreen, ScreenManager):
