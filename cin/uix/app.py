e184d4a565d09f0cf63d0cb3c2939c39ddd088e3
lluar <lluar@ccodein.org>
2022-07-12 18:45:25 +0200

    ADD: refs for short access to screen managers

--- cin/uix/app.py
+++ cin/uix/app.py
@@ -2,8 +2,8 @@
 from kivy.lang.builder import Builder
 from kivymd.uix.boxlayout import MDBoxLayout
 from kivymd.uix.toolbar import MDTopAppBar
-from kivymd.uix.navigationrail import MDNavigationRail
 from kivymd.uix.navigationdrawer import MDNavigationDrawer
+from kivymd.app import MDApp
 
 
 Builder.load_file('uix/app.kv')
@@ -11,6 +11,10 @@ Builder.load_file('uix/app.kv')
 
 class App(MDBoxLayout):
     """The application layout."""
+
+    def on_kv_post(self, widget):
+        app = MDApp.get_running_app()
+        app.refs['main_screen_manager'] = widget.ids.main_screen_manager
 
 
 class AppBar(MDTopAppBar):
@@ -20,4 +24,4 @@ class AppBar(MDTopAppBar):
 class AppNavigation(MDNavigationDrawer):
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
-        self.set_state('open')
+        self.set_state('close')
